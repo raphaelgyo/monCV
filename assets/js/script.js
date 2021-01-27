8a416e4a980bcc4a8400ac8fbd60b9e70af91b38
raphaelgyo <57181994+raphaelgyo@users.noreply.github.com>
2021-01-27 22:42:48 +0100

    intégration dark mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,19 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// DARK MODE
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle('dark-mode');
+    var element2 = document.getElementById('buttonSwitchMode');
+    if (element2.innerHTML == '<i class="fas fa-moon"></i>') {
+        element2.innerHTML = '<i class="fas fa-sun"></i>';
+        element2.classList.add('btn-light');
+        element2.classList.remove('btn-dark');
+    } else {
+        element2.innerHTML = '<i class="fas fa-moon"></i>';
+        element2.classList.add('btn-dark');
+        element2.classList.remove('btn-light');
+    }
+}
